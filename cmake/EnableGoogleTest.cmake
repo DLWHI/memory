8892f089915a736ccce9589381fc2c12070bc3df
dlwhi <samimmortal999@gmail.com>
2023-12-27 02:00:33 +0300

    fixed install

--- cmake/EnableGoogleTest.cmake
+++ cmake/EnableGoogleTest.cmake
@@ -6,6 +6,7 @@ include(FetchContent)
 FetchContent_Declare(
   googletest
   URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+  EXCLUDE_FROM_ALL
 )
 
 if (WIN32)
